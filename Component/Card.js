480f32097d96200d9b8d51f4ac7699b6aef6fee6
Suman028 <sumans@axioned.com>
2022-03-11 10:54:25 +0530

    fixing styling

--- Component/Card.js
+++ Component/Card.js
@@ -9,7 +9,7 @@ export default function Card(props) {
       </div>
       <div className="flex justify-between items-center text-gray-400 text-xs md:text-sm">
         <div className="flex justify-start items-center space-x-2 capitalize">
-          <img className="h-7 w-7 rounded-full object-cover" src={profile} alt="profile" />
+          <img className="h-7 w-7 rounded-full object-cover" src={profile} alt={name} />
           <p>{name}</p>
         </div>
         <p>{time} seconds ago</p>
